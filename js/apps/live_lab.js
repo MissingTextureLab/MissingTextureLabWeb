c14c6db6bf34c3c03a5782ef3f7842d0b1e1eef5
lanesvm <missingtexturelab@gmail.com>
2025-11-10 00:52:07 +0100

    Update live_lab.js

--- js/apps/live_lab.js
+++ js/apps/live_lab.js
@@ -64,7 +64,7 @@ async function getPatchCode(item) {
 // ==========================================================
 async function loadPortfolioCodes() {
   try {
-    const res = await fetch("./data/live-lab.json", { cache: "no-store" });
+    const res = await fetch("../../data/live-lab.json", { cache: "no-store" });
     if (!res.ok) throw new Error(`Error al cargar JSON (${res.status})`);
 
     portfolioCodes = await res.json();
